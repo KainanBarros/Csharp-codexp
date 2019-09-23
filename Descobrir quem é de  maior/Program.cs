c6c5176194af92cb360a5b27c23e95e33e5cd109
FIC <FIC@info.local>
2019-09-23 11:33:12 -0300

    Versão 3

---
+++ Descobrir quem é de  maior/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Descobrir_quem_é_de__maior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maiores de idade.");
+
+            int [] idade = new int [10];
+            int maior = 0;
+            int menor = 0;
+            int contador = 0;
+
+            while(contador <10){
+                Console.WriteLine("Digite sua idade");
+                idade  [contador] = int.Parse(Console.ReadLine());
+                
+                if(idade[contador] >=18){
+                    maior++;
+                }
+                else{
+                    menor++;
+                }
+                contador++;
+            }
+
+                System.Console.WriteLine($"Tem {maior} maiores de idade");
+                System.Console.WriteLine($"Tem {menor} menores de idade");
+        }
+    }
+}
---
+++ Media de notas/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Media_de_notas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Media de notas");
+            Console.WriteLine("---------------------------------------");
+
+            int [] nota1 = new int [10];
+            int [] nota2 = new int [10];
+            float total = 0;
+            int contador = 0;
+
+            while(contador < 10){
+                Console.WriteLine("Digite a 1° nota:");
+                nota1 [contador] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Digite a 2° nota:");
+                nota2 [contador] = int.Parse(Console.ReadLine());
+                contador++;
+            }
+            contador = 0;
+            while(contador < 10){
+                int somadoria = nota1 [contador] + nota2 [contador];
+                float resultado = somadoria/2;
+                total =+ resultado;
+                contador++;
+            if(resultado >= 7){
+                Console.WriteLine($"Você foi Aprovado.");
+            }
+            else{
+                Console.WriteLine("Você foi reprovado.");
+            }
+            }
+
+            Console.WriteLine($" {total/10}");
+        }
+    }
+}
